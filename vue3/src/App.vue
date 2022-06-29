97af8fa170b46e3311eebb589317e25d95d3de66
lyg0105 <lyg0105@naver.com>
2022-06-29 19:08:47 +0900

    vue ing2

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -6,20 +6,41 @@
   </div>
   <h1>부동산 리스트</h1>
   <div v-for="(product,i) in products" :key="i">
-    <h4>{{i+1}}. {{product}}</h4>
-    <p>50만원</p>
+    <img :src="require('@/assets/'+product.img)" class="room_img" >
+    <h4 @click="is_open_modal=true">{{i+1}}. {{product["name"]}}</h4>
+    <p>{{product["money"]}}만원</p>
+    <button @click="increase(i)">허위매물신고</button> 
+    <span>신고수: {{product["report"]}}</span>
   </div>
+
+<div class="black-bg" v-if="is_open_modal"  >
+  <div class="white-bg">
+    <h4>상세페이지임</h4>
+    <p>상세페이지 내용임</p>
+    <div>
+      <button @click="is_open_modal=false" >닫기</button>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
+import onerooms from './oneroom.js';
 
 export default {
   name: 'App',
   data(){
     return {
-      products:['신설동','종로1','역삼동'],
+      is_open_modal:false,
+      products:onerooms,
       menus:["Home","Shop","About"],
     };
+  },
+  methods: {
+    increase(i){
+      this.products[i]["report"]++;
+    }
   },
   components: {
     
@@ -28,6 +49,12 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+}
+div{
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -43,5 +70,25 @@ export default {
 .menu a{
   color:white;
   padding: 10px;
+}
+.room_img{
+  width: 100%;
+  max-width:200px;
+  margin-top: 40px;
+}
+.black-bg{
+  width: 100%;
+  height: 100%;
+  background: #111;
+  position: fixed;
+  padding:20px;
+  left: 0px;
+  top:0px;
+}
+.white-bg{
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding:20px;
 }
 </style>
---
+++ vue3/src/oneroom.js
@@ -0,0 +1,22 @@
+var onerooms=[
+  {
+    "name":"신설동",
+    "money":50,
+    "report":0,
+    "img":"11111.jpg"
+  },
+  {
+    "name":"종로1",
+    "money":60,
+    "report":0,
+    "img":"111561_95843_1910.jpg"
+  },
+  {
+    "name":"역삼동",
+    "money":70,
+    "report":0,
+    "img":"LGvMiFn7_400x400.jpg"
+  }
+];
+
+export default onerooms;
