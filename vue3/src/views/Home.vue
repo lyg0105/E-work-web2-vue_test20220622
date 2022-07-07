d2a8ef66acc5d5b446158594f600336b3ffe1c71
lyg0105 <lyg0105@naver.com>
2022-07-07 12:33:40 +0900

    vuex test

--- vue3/src/views/Home.vue
+++ vue3/src/views/Home.vue
@@ -1,8 +1,27 @@
 <template>
   <h1>Home Page</h1>
+  {{ counter }}
+  {{ times2 }}
+  <button @click="inc">inc</button>
+  {{m_count}}
 </template>
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
-  name: 'MyHome'
+  name: 'MyHome',
+  computed:{
+    m_count(){
+      return this.$store.state.counter
+    }
+  },
+  setup() {
+    const store = useStore();
+    const counter = computed(() => store.state.counter);
+    const test = computed(() => store.getters);
+    const inc = () => store.commit("setCounter", counter.value + 1);
+
+    return { counter, inc, test };
+  }
 }
 </script>
